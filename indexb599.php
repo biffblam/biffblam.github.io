f0fe89dfc29e8f95d9b8722fb8e74e70c69e8948
belzecue <1931303+belzecue@users.noreply.github.com>
2025-06-12 23:17:00 +0800

    init.

---
+++ indexb599.php
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="0.92" xml:lang="en-US">
+	<channel>
+		<title>The 10-Page Torture Test</title>
+		<link>https://10ptt.com/smf/index.php</link>
+		<description><![CDATA[Live information from The 10-Page Torture Test]]></description>
+		<item>
+			<title><![CDATA[Re: 08PTT: Exposure by Anna C. Webster (Resident Evil spec)]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=410.msg1734#msg1734</link>
+			<description><![CDATA[TO BE CONTINUED...]]></description>
+			<category><![CDATA[10PTTs]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=410.0</comments>
+			<pubDate>Sat, 19 Apr 2025 17:04:11 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=410.msg1734#msg1734</guid>
+		</item>
+		<item>
+			<title><![CDATA[Re: 08PTT: Exposure by Anna C. Webster (Resident Evil spec)]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=410.msg1733#msg1733</link>
+			<description><![CDATA[[img]]>&#093;<![CDATA[https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjgdN1r9Roeu9nO3__BF6hNiWJGjSUzejHT96TYzme0DJzi5TBKgL2U4nrU7qE-_Nqty71wyOaQ-7fltWQmu0u83gcJsYvB0RqeX8M7zi9odxv6juyMloyrUcUKJl49wnW8p0yLt5nNDXEpHmnJbY1gpUzrxbgavSgypMEzQeg2Wx21r8ai4RjAyMlaoS...]]></description>
+			<category><![CDATA[10PTTs]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=410.0</comments>
+			<pubDate>Sat, 19 Apr 2025 16:50:04 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=410.msg1733#msg1733</guid>
+		</item>
+		<item>
+			<title><![CDATA[08PTT: Exposure by Anna C. Webster (Resident Evil spec)]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=410.msg1731#msg1731</link>
+			<description><![CDATA[<a href="https://annacwebster.com/" target="_blank">Anna C. Webster</a> is an award-winner writer and narrative designer who popped up on my gamedev Bluesky feeds. &nbsp;She offers a screenplay writing sample online. &nbsp;I read a couple pages and knew immediately I had to feed i...]]></description>
+			<category><![CDATA[10PTTs]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=410.0</comments>
+			<pubDate>Sun, 13 Apr 2025 15:59:18 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=410.msg1731#msg1731</guid>
+		</item>
+		<item>
+			<title><![CDATA[WE'RE ALL GONNA DIE - RocketJump Resurrection]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=409.msg1730#msg1730</link>
+			<description>
+<![CDATA[<a href="https://en.wikipedia.org/wiki/Freddie_Wong" target="_blank">Freddie&#39;s</a> back.<br /><br />[html]]>&#093;<![CDATA[<iframe width="560" height="315" src="https://www.youtube.com/embed/zoYVfdP0oOc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; e...]]>
+			</description>
+			<category><![CDATA[Loglines]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=409.0</comments>
+			<pubDate>Thu, 27 Feb 2025 19:09:37 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=409.msg1730#msg1730</guid>
+		</item>
+		<item>
+			<title><![CDATA[STING by Kiah Roache-Turner (2024)]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=408.msg1729#msg1729</link>
+			<description>
+<![CDATA[<a href="https://www.imdb.com/title/tt20112746/" target="_blank">https://www.imdb.com/title/tt20112746/</a><br /><br /><b>Logline</b><br /><br />&quot;After raising an unnervingly talented spider in secret, 12-year-old Charlotte must face the facts about her pet-and fight for her family&#39;s survival-when the once-charming creature rapidly transform...]]>
+			</description>
+			<category><![CDATA[Loglines]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=408.0</comments>
+			<pubDate>Tue, 30 Apr 2024 12:36:30 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=408.msg1729#msg1729</guid>
+		</item>
+	</channel>
+</rss>
---
+++ indexee76.php
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="0.92" xml:lang="en-US">
+	<channel>
+		<title>The 10-Page Torture Test</title>
+		<link>https://10ptt.com/smf/index.php</link>
+		<description><![CDATA[Live information from The 10-Page Torture Test]]></description>
+		<item>
+			<title><![CDATA[Re: 08PTT: Exposure by Anna C. Webster (Resident Evil spec)]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=410.msg1734#msg1734</link>
+			<description><![CDATA[TO BE CONTINUED...]]></description>
+			<category><![CDATA[10PTTs]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=410.0</comments>
+			<pubDate>Sat, 19 Apr 2025 17:04:11 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=410.msg1734#msg1734</guid>
+		</item>
+		<item>
+			<title><![CDATA[Re: 08PTT: Exposure by Anna C. Webster (Resident Evil spec)]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=410.msg1733#msg1733</link>
+			<description><![CDATA[[img]]>&#093;<![CDATA[https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjgdN1r9Roeu9nO3__BF6hNiWJGjSUzejHT96TYzme0DJzi5TBKgL2U4nrU7qE-_Nqty71wyOaQ-7fltWQmu0u83gcJsYvB0RqeX8M7zi9odxv6juyMloyrUcUKJl49wnW8p0yLt5nNDXEpHmnJbY1gpUzrxbgavSgypMEzQeg2Wx21r8ai4RjAyMlaoS...]]></description>
+			<category><![CDATA[10PTTs]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=410.0</comments>
+			<pubDate>Sat, 19 Apr 2025 16:50:04 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=410.msg1733#msg1733</guid>
+		</item>
+		<item>
+			<title><![CDATA[08PTT: Exposure by Anna C. Webster (Resident Evil spec)]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=410.msg1731#msg1731</link>
+			<description><![CDATA[<a href="https://annacwebster.com/" target="_blank">Anna C. Webster</a> is an award-winner writer and narrative designer who popped up on my gamedev Bluesky feeds. &nbsp;She offers a screenplay writing sample online. &nbsp;I read a couple pages and knew immediately I had to feed i...]]></description>
+			<category><![CDATA[10PTTs]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=410.0</comments>
+			<pubDate>Sun, 13 Apr 2025 15:59:18 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=410.msg1731#msg1731</guid>
+		</item>
+		<item>
+			<title><![CDATA[WE'RE ALL GONNA DIE - RocketJump Resurrection]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=409.msg1730#msg1730</link>
+			<description>
+<![CDATA[<a href="https://en.wikipedia.org/wiki/Freddie_Wong" target="_blank">Freddie&#39;s</a> back.<br /><br />[html]]>&#093;<![CDATA[<iframe width="560" height="315" src="https://www.youtube.com/embed/zoYVfdP0oOc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; e...]]>
+			</description>
+			<category><![CDATA[Loglines]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=409.0</comments>
+			<pubDate>Thu, 27 Feb 2025 19:09:37 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=409.msg1730#msg1730</guid>
+		</item>
+		<item>
+			<title><![CDATA[STING by Kiah Roache-Turner (2024)]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=408.msg1729#msg1729</link>
+			<description>
+<![CDATA[<a href="https://www.imdb.com/title/tt20112746/" target="_blank">https://www.imdb.com/title/tt20112746/</a><br /><br /><b>Logline</b><br /><br />&quot;After raising an unnervingly talented spider in secret, 12-year-old Charlotte must face the facts about her pet-and fight for her family&#39;s survival-when the once-charming creature rapidly transform...]]>
+			</description>
+			<category><![CDATA[Loglines]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=408.0</comments>
+			<pubDate>Tue, 30 Apr 2024 12:36:30 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=408.msg1729#msg1729</guid>
+		</item>
+		<item>
+			<title><![CDATA[Resurrection  2021]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=407.msg1728#msg1728</link>
+			<description><![CDATA[Having shuttered the site for some years after linked images broke when Wordpress changed their hosting policies, I recently decided to reopen the site.&nbsp; Reopen the window, that is, not the door.&nbsp; Registration is off and all membership is wiped.&nbsp; Going...]]></description>
+			<category><![CDATA[Site News &amp; Info]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=407.0</comments>
+			<pubDate>Sat, 07 Aug 2021 08:34:18 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=407.msg1728#msg1728</guid>
+		</item>
+		<item>
+			<title><![CDATA[Adult Swim's BLACK LOTUS]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=406.msg1727#msg1727</link>
+			<description>
+<![CDATA[Super excited for this one, arriving in a couple months. &nbsp;And that logline! &nbsp;Really hits the high notes: place, time, protag and their problem, and what to expect:<br /><br /><div class="quoteheader">Quote</div><div class="quote">&quot;Los Angeles 2032. A young woman wakes up with no memories, and possessing deadly...</div>]]>
+			</description>
+			<category><![CDATA[Loglines]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=406.0</comments>
+			<pubDate>Sat, 07 Aug 2021 08:06:49 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=406.msg1727#msg1727</guid>
+		</item>
+		<item>
+			<title><![CDATA[Re: Spec Logline Workshop, GITS Jan-Jun 2017]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=405.msg1726#msg1726</link>
+			<description><![CDATA[RIP, John McAfee, you strange, flawed, larger-than-life, and possibly murderous human being.]]></description>
+			<category><![CDATA[Loglines]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=405.0</comments>
+			<pubDate>Fri, 25 Jun 2021 04:28:41 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=405.msg1726#msg1726</guid>
+		</item>
+		<item>
+			<title><![CDATA[Re: Spec Logline Workshop, GITS Jan-Jun 2017 - Title: World Breaker]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=405.msg1725#msg1725</link>
+			<description><![CDATA[<div class="quoteheader"><a href="https://10ptt.com/smf/index.php?topic=405.msg1722#msg1722">Quote from: Pitchpatch on July 25, 2017, 04:25 PM</a></div><div class="quote"><b>Logline: In the near future, after creatures from an alternate world attack Earth, a father hides his daughter so she won&#39;t be conscripted into humanity&#39;s only defense: an all-...</b></div>]]></description>
+			<category><![CDATA[Loglines]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=405.0</comments>
+			<pubDate>Fri, 25 Jun 2021 04:25:19 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=405.msg1725#msg1725</guid>
+		</item>
+		<item>
+			<title><![CDATA[Re: Spec Logline Workshop, GITS Jan-Jun 2017]]></title>
+			<link>https://10ptt.com/smf/index.php?topic=405.msg1724#msg1724</link>
+			<description><![CDATA[Some damn good work in these thar posts. Nice job, sir! Still not sure why I don&#39;t get notified of everything you post. Going to remedy that once and for all, right now.]]></description>
+			<category><![CDATA[Loglines]]></category>
+			<comments>https://10ptt.com/smf/index.php?action=post;topic=405.0</comments>
+			<pubDate>Mon, 25 Jun 2018 20:36:46 GMT</pubDate>
+			<guid>https://10ptt.com/smf/index.php?topic=405.msg1724#msg1724</guid>
+		</item>
+	</channel>
+</rss>
